93e7514d71393f9421b525b8d6cd735a31f1ea26
Galvanize <Galvanize@Brendans-MacBook-Air.local>
2016-08-12 11:49:26 -0600

    add structure

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,5 +1,28 @@
 (function(){
+  HomeController.$inject = ['$scope'];
+  BioController.$inject = ['$scope'];
+  ProjectsController.$inject = ['$scope'];
+  ResumeController.$inject = ['$scope'];
 
+  function HomeController($scope){
+    $scope.view = {};
+    $scope.view.message = 'This is the Home page.'
+  }
+  function BioController($scope){
+    $scope.view = {};
+    $scope.view.message = 'This is the Bio page.'
+  }
+  function ProjectsController($scope){
+    $scope.view = {};
+    $scope.view.message = 'This is the Projects page.'
+  }
+  function ResumeController($scope){
+    $scope.view = {};
+    $scope.view.message = 'This is the Resume page.'
+  }
 
-  angular.module('').controller('', );
+  angular.module('portfolio').controller('HomeController', HomeController);
+  angular.module('portfolio').controller('BioController', BioController);
+  angular.module('portfolio').controller('ProjectsController', ProjectsController);
+  angular.module('portfolio').controller('ResumeController', ResumeController);
 }());
